122fb3555ad39bfda8ae3145372495dc06dac0fc
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2026-02-23 13:16:02 +0900

    에셋추가

--- 3DNetworkGame/Assets/02.Scripts/Core/PhotonServerManager.cs
+++ 3DNetworkGame/Assets/02.Scripts/Core/PhotonServerManager.cs
@@ -1,5 +1,6 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PhotonServerManager : MonoBehaviourPunCallbacks
@@ -61,12 +62,33 @@ public class PhotonServerManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("룸 입장 완료!");
+
+        Debug.Log($"룸:{ PhotonNetwork.CurrentRoom.Name}");
+        Debug.Log($"플레이어 인원:{ PhotonNetwork.CurrentRoom.PlayerCount}");
+
+        //룸에 입장한 플레이어 정보
+        Dictionary<int,Player> roomPlayers = PhotonNetwork.CurrentRoom.Players;
+        foreach (KeyValuePair<int, Player> player in roomPlayers)
+        {
+            Debug.Log($"{player.Value.NickName} : {player.Value.ActorNumber}");
+        }
     }
 
     //랜덤 방 입장에 실패하면 자동으로 호출되는 콜백함수
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log($"랜덤 방 입장에 실패했습니다:{returnCode} - {message}");
+
+        //랜덤 룸 입장에 실패하면.. 룸이 하나도 없는 것이지... 룸을 만들자!
+
+        //룸 옵션 정의
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 20;    // 룸 최대 접속자 수
+        roomOptions.IsVisible = true;   // 로비에서 룸을 보여줄 것인지
+        roomOptions.IsOpen = true;      // 룸의 오픈 여부
+
+        //룸 만들기
+        PhotonNetwork.CreateRoom("test", roomOptions);
     }
     
     //방 입장에 실패하면 자동으로 호출되는 콜백함수
